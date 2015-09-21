72bef9d2d9b1192d3900c3d66d9ccefabca79503
Aditya Khanna <khanna7@uchicago.edu>
2015-09-21 15:32:48 -0500

    steve's modifications

--- explore_nodefactor_on_R0net.R
+++ explore_nodefactor_on_R0net.R
@@ -13,43 +13,22 @@
    length.of.class <- 7
    deg.R0.net.sort <- sort(deg.R0.net, decreasing=FALSE, index=TRUE)
 
-   # deg.R0.net.class.mat <- matrix(deg.R0.net.sort$x, ncol=length.of.class, byrow=TRUE)
-   # deg.R0.net.class.means <- apply(deg.R0.net.class.mat, 1, function(x)
-   # mean(x, ndeg.R0.net.rm=TRUE))
-   #deg.R0.net.sort.index <- deg.R0.net.sort$ix
+   n <- network.size(R0.net)
+   num.classes <- floor(n/length.of.class)
+   remainder <- n - length.of.class*num.classes
+   deg.R0.net.deg.class <- rep(1:num.classes, each=length.of.class)
+   deg.R0.net.deg.class <- c(deg.R0.net.deg.class, rep(num.classes, remainder))
+   deg.R0.net.deg.class[deg.R0.net.sort$ix] <- deg.R0.net.deg.class
    
-   #deg.R0.net.deg.class <- rep(-1, length(deg.R0.net))
-   
-   #for (i in 1:length(deg.R0.net.deg.class)){
-   #    deg.R0.net.deg.class[i] <- deg.R0.net.class.means[((deg.R0.net.sort.index[i] %/% length.of.class)+1)]
-   #}
-  
-   # Aditya, your apprach meant that the last row got padded with 0s, which were then included in the mean calculation
+   table(deg.R0.net.deg.class)
+   R0.net %v% "deg.class.mean.deg" <- deg.R0.net.deg.class
 
-  n <- network.size(R0.net)
-  num.classes <- floor(n/length.of.class)
-  remainder <- n - length.of.class*num.classes
-  deg.R0.net.deg.class <- rep(1:num.classes, each=length.of.class)
-  deg.R0.net.deg.class <- c(deg.R0.net.deg.class, rep(num.classes, remainder))
-  deg.R0.net.deg.class[deg.R0.net.sort$ix] <- deg.R0.net.deg.class
-  #class.means <- sapply(1:num.classes, function(x) mean(deg.R0.net[deg.R0.net.deg.class==x]))
-  #class.sizes <- sapply(1:num.classes, function(x) sum(deg.R0.net.deg.class==x))
-  #nodefactor.stats <- class.means*class.sizes           
-   
-  table(deg.R0.net.deg.class)
-  R0.net %v% "deg.class.mean.deg" <- deg.R0.net.deg.class
-
-  ## fit edges-only model
-  edges.mod <- ergm(R0.net ~ edges)
-  summary(edges.mod)
-  edges.mod.gof.deg <- gof(edges.mod ~ degree, control=control.gof.formula(nsim=10), verbose=T)
-  plot(edges.mod.gof.deg)
+ ## fit edges-only model
+   edges.mod <- ergm(R0.net ~ edges)
+   summary(edges.mod)
+   edges.mod.gof.deg <- gof(edges.mod ~ degree, control=control.gof.formula(nsim=10), verbose=T)
+   plot(edges.mod.gof.deg)
  
-  ## fit edges+nodecov model
-  #edges.nodecov.mod <- ergm(R0.net ~ edges+nodecov("deg.class.mean.deg"))
-  #summary(edges.nodecov.mod)
-  #edges.nodecov.mod.gof.deg <- gof(edges.nodecov.mod ~ degree)
-  #plot(edges.nodecov.mod.gof.deg)
 
   ## fit edges+nodefactor model
   edges.nodef.mod <- ergm(R0.net ~ edges+nodefactor("deg.class.mean.deg", base=10))
