0eda632eb89cf74e7985f4d996285c95b5eb8883
Aditya Khanna <khanna7@uchicago.edu>
2016-02-19 13:39:20 -0600

    needed to compute 'respdeg' correctly. made change. imputation of one network looks good now

--- empirical/wave1/fit-ergm-to-imputed-network.R
+++ empirical/wave1/fit-ergm-to-imputed-network.R
@@ -12,11 +12,24 @@
    ## Add 'respdeg' attribute
    deg.imputed_network <- degree(imputed_network)
    imputed_network%v%"respdeg" <- deg.imputed_network
+   n <- network.size(imputed_network)
+   resp <- union(
+                 which(substr(imputed_network %v% "vertex.names",1,4) == "1111"),
+                 which(substr(imputed_network %v% "vertex.names",1,4) == "2222")
+                 )
+
+   set.vertex.attribute(imputed_network, "respondent", 0)
+   set.vertex.attribute(imputed_network, "respondent", 1, v=resp)
+   nresp <- length(resp)
+
+   respdeg <- sapply(1:n, function(x) sum(imputed_network[x,1:nresp]))
+   imputed_network%v%"respdeg" <- respdeg
 
    ## Fit ergm with edges + nodecov('respdeg')
    ergm.imputed_network <- ergm(imputed_network ~ edges +
                                                   nodecov('respdeg'),
-                                                  verbose=TRUE)
+                                                  verbose=TRUE
+                                )
                                             
    ## save
    save.image("fitted_ergm_imputed_network.RData")
--- empirical/wave1/simulate-from-fitted-ergm-in-serial.R
+++ empirical/wave1/simulate-from-fitted-ergm-in-serial.R
@@ -1,13 +1,20 @@
 ## Simulate from fitted ergm object
    rm(list=ls())
    ## libraries
+   library(network)
    library(ergm)
 
    ## data
    load("fitted_ergm_imputed_network.RData")
+   #load("fitted_ergm_imputed_network-lower-missing-alters.RData")
 
    ## Simulate from fitted ERGM
-   sim <- simulate(ergm.imputed_network, constraints=~observed, nsim=1)
+   sim <- simulate(ergm.imputed_network, constraints=~observed, nsim=1,
+                   control=control.simulate.ergm(
+                       MCMC.burnin=5e8,
+                       MCMC.interval=5e8
+                   )
+                   )
    sim #simualted object
    
    ## save
