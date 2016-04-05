03eecbc4c2f72d33248cf67e40f7eb197f20f9d1
Aditya Khanna <khanna7@uchicago.edu>
2016-04-05 11:44:04 -0500

    names in numeric format for finding names > 298

--- empirical/wave1/compute-btwn-on-dyad-ind-igraphs.R
+++ empirical/wave1/compute-btwn-on-dyad-ind-igraphs.R
@@ -26,7 +26,9 @@
                                                      decreasing=TRUE)
    length(sort_top300_btwn_dyadic_ind_mod_base_13)
 
-   length(which(names(sort_top300_btwn_dyadic_ind_mod_base_13) > 298))
+   names.in.num <- as.numeric(names(sort_top300_btwn_dyadic_ind_mod_base_13))
+   length(which(names.in.num > 298))
 
    ## save 
    save.image(file="sort_top300_btwn_dyadic_ind_mod_base_13.RData")
+   saveRDS(sort_top300_btwn_dyadic_ind_mod_base_13, file="top300_btwn_dyad_ind.RDS")
