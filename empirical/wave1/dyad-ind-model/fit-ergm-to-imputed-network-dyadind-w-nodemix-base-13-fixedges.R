9202982408a7ff30a15df1bce95db03b7736ac5e
Aditya Khanna <khanna7@uchicago.edu>
2016-05-21 12:47:28 -0500

    move independent model fits to  ind model folder

---
+++ empirical/wave1/dyad-ind-model/fit-ergm-to-imputed-network-dyadind-w-nodemix-base-13-fixedges.R
@@ -0,0 +1,36 @@
+## create network with unobserved edges from wave 1
+   rm(list=ls())
+
+   ## libraries
+   library(network)
+   library(ergm)
+   library(sna)
+
+   ## data
+   load(file="imputed_network.RData")
+
+   ## Add 'respdeg' attribute
+   deg.imputed_network <- degree(imputed_network)
+   #imputed_network%v%"respdeg" <- deg.imputed_network (was incorrect)
+   n <- network.size(imputed_network)
+   resp <- union(
+                 which(substr(imputed_network %v% "vertex.names",1,4) == "1111"),
+                 which(substr(imputed_network %v% "vertex.names",1,4) == "2222")
+                 )
+
+   set.vertex.attribute(imputed_network, "respondent", 0)
+   set.vertex.attribute(imputed_network, "respondent", 1, v=resp)
+   nresp <- length(resp)
+
+   respdeg <- sapply(1:n, function(x) sum(imputed_network[x,1:nresp]))
+   imputed_network%v%"respdeg" <- respdeg
+
+   ## Fit ergm with edges + nodecov('respdeg')
+   ergm.imputed_network <- ergm(imputed_network ~ nodecov('respdeg')+
+                                                  nodemix('respondent',
+                                                          base=c(1,3)),
+                                                  verbose=TRUE
+                                )
+                                            
+   ## save
+   save.image("fitted_ergm_imputed_network_dyadind_nodemix_base13.RData")
