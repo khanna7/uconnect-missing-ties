adf66eaa663f33cafd83397f9812a404adf84386
Aditya Khanna <khanna7@uchicago.edu>
2016-05-22 10:35:37 -0500

    change name of saveRDS file to save and compute summaries

--- empirical/wave1/dyad-ind-model/sim-from-ergm/save-sim-with-nodemix-in-RDS-form-and-comp-summaries.R
+++ empirical/wave1/dyad-ind-model/sim-from-ergm/save-sim-with-nodemix-in-RDS-form-and-comp-summaries.R
@@ -14,9 +14,20 @@
    sim_results[[1]]
 
    ## save 100
+   set.seed(7)
    sel_100_nets <- sample(sim_results, 100)
    saveRDS(sel_100_nets, "imputed_100_nets_w_nodemix.RDS")
 
    ## save 10
    sel_10_nets <- sample(sim_results, 10)
    saveRDS(sel_10_nets, "imputed_10_nets_w_nodemix.RDS")
+
+   ## compute mean statistics on generated networks
+      ## 100 networks
+
+
+      ## 10 networks
+      ecount_10_nets <- unlist(lapply(sel_10_nets, network.edgecount))
+      summary(ecount_10_nets) #imputed networks, incl. all ties
+      summary(ecount_10_nets - 29700) #imputed networks, only NR-NR ties
+
