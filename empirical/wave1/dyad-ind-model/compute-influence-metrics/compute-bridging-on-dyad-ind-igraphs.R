8e91bb87437e6d6ea3839a2ebf2717b615063305
Aditya Khanna <khanna7@uchicago.edu>
2016-05-22 12:01:10 -0500

    removed excess bridging files

--- empirical/wave1/dyad-ind-model/compute-influence-metrics/compute-bridging-on-dyad-ind-igraphs.R
+++ empirical/wave1/dyad-ind-model/compute-influence-metrics/compute-bridging-on-dyad-ind-igraphs.R
@@ -4,26 +4,33 @@
    rm(list=ls())
    library(igraph)
    library(influenceR)
-   load("dyadic-ind-mod-base-13-igraphs.RData")
+
+   igraph_list <- readRDS("ten_imputed_igraphs.RDS")
 
    ## compute betweenness centrality
-   bridge_dyadic_ind_mod_base_13 <- lapply(sim_results_igraph_indmod_base13,
+   bridge_dyadic_ind_mod_base_13 <- lapply(igraph_list,
                                            function(x)
                                                bridging(x))
 
-   ordered_bridge_dyadic_ind_mod_base_13 <- lapply(bridge_dyadic_ind_mod_base_13,
-                                                   function (x)
-                                                       order(x, decreasing=TRUE)
-                                                   )
+   ordered_bridge_dyadic_ind_mod_base_13 <-
+       lapply(bridge_dyadic_ind_mod_base_13,
+              function (x)
+                  order(x, decreasing=TRUE)
+              )
 
-   top300_bridge_dyadic_ind_mod_base_13 <- lapply(ordered_bridge_dyadic_ind_mod_base_13,
+   top300_bridge_dyadic_ind_mod_base_13 <-
+       lapply(ordered_bridge_dyadic_ind_mod_base_13,
                                                   function (x)
                                                       x[1:300]
                                                   )
 
-   freq_top300_bridge_dyadic_ind_mod_base_13 <- table(unlist(top300_bridge_dyadic_ind_mod_base_13))
-   sort_top300_bridge_dyadic_ind_mod_base_13 <- sort(freq_top300_bridge_dyadic_ind_mod_base_13,
-                                                     decreasing=TRUE)
+   freq_top300_bridge_dyadic_ind_mod_base_13 <-
+       table(unlist(top300_bridge_dyadic_ind_mod_base_13))
+
+   sort_top300_bridge_dyadic_ind_mod_base_13 <-
+       sort(freq_top300_bridge_dyadic_ind_mod_base_13,
+            decreasing=TRUE)
+
    length(sort_top300_bridge_dyadic_ind_mod_base_13)
 
    length(which(names(sort_top300_bridge_dyadic_ind_mod_base_13) > 298))
