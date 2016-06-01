76c1bce935061bf8ccbce4aa4097bb5596d4075e
Aditya Khanna <khanna7@uchicago.edu>
2016-06-01 10:54:32 -0500

    stability computations to include nodes from imputed networks after cutoffs

--- empirical/wave1/dyad-ind-model/summarize-centrality-results/node-stability-within-measure-btwn-control-and-imputations.R
+++ empirical/wave1/dyad-ind-model/summarize-centrality-results/node-stability-within-measure-btwn-control-and-imputations.R
@@ -2,51 +2,58 @@
 ## and imputed networks for each measure
 
 rm(list=ls())
+load("resp_pcas_after_cutoffs.RData")
 
    ## betweenness
    btwn_control_885nodes <- readRDS("../../control-net/control_885nodes_btwn_top300.RDS")
-   btwn_imputed_885nodes <- readRDS("btwn_top300_ids.RDS")
 
    resp_btwn_control_885nodes <- btwn_control_885nodes[which(btwn_control_885nodes <= 298)]
-   resp_btwn_imputed_885nodes <- btwn_imputed_885nodes[which(btwn_imputed_885nodes <= 298)]
-   resp_sel_in_raw_imputed_btwn <- intersect(resp_btwn_control_885nodes, resp_btwn_imputed_885nodes)
-   length(resp_sel_in_raw_imputed_btwn)
+   
+   resp_sel_in_control_imputed_btwn <- intersect(resp_btwn_control_885nodes, 
+                                                 resp_btwn_after_cutoff)
+   length(resp_sel_in_control_imputed_btwn)
 
-   btwn_control_imputed <- intersect(btwn_control_885nodes, btwn_imputed_885nodes)
-
+   btwn_control_imputed <- intersect(btwn_control_885nodes, btwn_after_cutoff)
+   length(btwn_control_imputed)
+   
    ## eigenvector centrality
    evcent_control_885nodes <- readRDS("../../control-net/control_885nodes_evcent_top300.RDS")
-   evcent_imputed_885nodes <- readRDS("evcent_top300_ids.RDS")
 
    resp_evcent_control_885nodes <- evcent_control_885nodes[which(evcent_control_885nodes <= 298)]
-   resp_evcent_imputed_885nodes <- evcent_imputed_885nodes[which(evcent_imputed_885nodes <= 298)]
-   resp_sel_in_raw_imputed_evcent <- intersect(resp_evcent_control_885nodes, resp_evcent_imputed_885nodes)
-   length(resp_sel_in_raw_imputed_evcent)
 
-   evcent_control_imputed <- intersect(evcent_control_885nodes, evcent_imputed_885nodes)
+   resp_sel_in_control_imputed_evcent <- intersect(resp_evcent_control_885nodes, 
+                                              resp_evcent_after_cutoff)
+   length(resp_sel_in_control_imputed_evcent)
+
+   evcent_control_imputed <- intersect(evcent_control_885nodes, evcent_after_cutoff)
+   length(evcent_control_imputed)
+
    ## bridging
    bridging_control_885nodes <- readRDS("../../control-net/control_885nodes_bridging_top300.RDS")
-   bridging_imputed_885nodes <- readRDS("bridging_top300_ids.RDS")
 
    resp_bridging_control_885nodes <- bridging_control_885nodes[which(bridging_control_885nodes <= 298)]
-   resp_bridging_imputed_885nodes <- bridging_imputed_885nodes[which(bridging_imputed_885nodes <= 298)]
-   resp_sel_in_raw_imputed_bridging <- intersect(resp_bridging_control_885nodes, resp_bridging_imputed_885nodes)
-   length(resp_sel_in_raw_imputed_bridging)
 
-   bridging_control_imputed <- intersect(bridging_control_885nodes, bridging_imputed_885nodes)
+   resp_sel_in_control_imputed_bridging <- intersect(resp_bridging_control_885nodes, 
+                                                resp_bridging_after_cutoff)
+   length(resp_sel_in_control_imputed_bridging)
+
+   bridging_control_imputed <- intersect(bridging_control_885nodes, bridging_after_cutoff)
+   length(bridging_control_imputed)
+
    
    ## key player
    kp_control_885nodes <- readRDS("../../control-net/control_885nodes_kp_set300.RDS")
-   kp_imputed_885nodes <- readRDS("set300_kp_dyad_ind.RDS")
 
    resp_kp_control_885nodes <- kp_control_885nodes[which(kp_control_885nodes <= 298)]
-   resp_kp_imputed_885nodes <- kp_imputed_885nodes[which(kp_imputed_885nodes <= 298)]
-   resp_sel_in_raw_imputed_kp <- intersect(resp_kp_control_885nodes, resp_kp_imputed_885nodes)
-   length(resp_sel_in_raw_imputed_kp)
 
-   kp_control_imputed <- intersect(kp_control_885nodes, kp_imputed_885nodes)
+   resp_sel_in_control_imputed_kp <- intersect(resp_kp_control_885nodes, 
+                                           resp_kp_after_cutoff)
+   length(resp_sel_in_control_imputed_kp)
 
-   ## overlaps in these pCA's on the control (i.e. raw) and imputed networks
+   kp_control_imputed <- intersect(kp_control_885nodes, kp_after_cutoff)
+   length(kp_control_imputed)
+
+   ## overlaps in these pCA's on the control (i.e. control) and imputed networks
       ## btwn and all three
       length(intersect(btwn_control_imputed, evcent_control_imputed))
       length(intersect(btwn_control_imputed, kp_control_imputed))
@@ -59,16 +66,18 @@ rm(list=ls())
       ## bridging and one
       length(intersect(bridging_control_imputed, kp_control_imputed))
 
-
-   ## overlaps in these respondent pCA's on the control (i.e. raw) and imputed networks
+   ## overlaps in these respondent pCA's on the control (i.e. control) and imputed networks
       ## btwn and all three
-      length(intersect(resp_sel_in_raw_imputed_btwn, resp_sel_in_raw_imputed_evcent))
-      length(intersect(resp_sel_in_raw_imputed_btwn, resp_sel_in_raw_imputed_kp))
-      length(intersect(resp_sel_in_raw_imputed_btwn, resp_sel_in_raw_imputed_bridging))
+      length(intersect(resp_sel_in_control_imputed_btwn, resp_sel_in_control_imputed_evcent))
+      length(intersect(resp_sel_in_control_imputed_btwn, resp_sel_in_control_imputed_kp))
+      length(intersect(resp_sel_in_control_imputed_btwn, resp_sel_in_control_imputed_bridging))
 
       ## evcent and two
-      length(intersect(resp_sel_in_raw_imputed_evcent, resp_sel_in_raw_imputed_bridging))
-      length(intersect(resp_sel_in_raw_imputed_evcent, resp_sel_in_raw_imputed_kp))
+      length(intersect(resp_sel_in_control_imputed_evcent, resp_sel_in_control_imputed_bridging))
+      length(intersect(resp_sel_in_control_imputed_evcent, resp_sel_in_control_imputed_kp))
 
       ## bridging and one
-      length(intersect(resp_sel_in_raw_imputed_bridging, resp_sel_in_raw_imputed_kp))
+      length(intersect(resp_sel_in_control_imputed_bridging, resp_sel_in_control_imputed_kp))
+
+      ## save object
+      save.image("node-stability-witin-measure-btwn-control-and-imputations.RData")
