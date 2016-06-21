018962645853bc27c18df87c5f3cd095b8f7403b
Aditya Khanna <khanna7@uchicago.edu>
2016-06-21 10:28:39 -0500

    0 tolerance computation on imputed graphs

---
+++ empirical/wave1/dyad-ind-model/compute-influence-metrics/compute-kp-0-tol-on-dyad-ind-igraphs.R
@@ -0,0 +1,17 @@
+## compute eigenvector centrality scores on dyadic independent igraphs
+
+   ## libraries and data
+   rm(list=ls())
+   library(igraph)
+   library(influenceR)
+
+   igraph_list <- readRDS("ten_imputed_igraphs.RDS")
+
+   ## compute betweenness centrality
+   kp_dyadic_ind_mod_base_13 <- lapply(igraph_list,
+                                           function(x)
+                                               keyplayer(x, k=300,
+                                                         tol=0,
+                                                         maxsex=24*3600))
+   ## save 
+   save.image(file="sort_top300_kp_dyadic_ind_mod_base_13.RData")
