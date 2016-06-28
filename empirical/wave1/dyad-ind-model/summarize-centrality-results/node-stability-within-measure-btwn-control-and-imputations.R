498f75ce66b72905caa0127f9ed7a2b5c8c16d0a
Aditya Khanna <khanna7@uchicago.edu>
2016-06-28 10:12:55 -0500

    node stability for nonrespondents

--- empirical/wave1/dyad-ind-model/summarize-centrality-results/node-stability-within-measure-btwn-control-and-imputations.R
+++ empirical/wave1/dyad-ind-model/summarize-centrality-results/node-stability-within-measure-btwn-control-and-imputations.R
@@ -4,20 +4,27 @@
 rm(list=ls())
 load("resp_pcas_after_cutoffs.RData")
 
+nresp <- 298
+
    ## betweenness
    btwn_control_885nodes <- readRDS("../../control-net/control_885nodes_btwn_top300.RDS")
+   nonresp_btwn_after_cutoff <- btwn_after_cutoff[btwn_after_cutoff > 298]
 
    resp_btwn_control_885nodes <- btwn_control_885nodes[which(btwn_control_885nodes <= 298)]
-   
+
    resp_sel_in_control_imputed_btwn <- intersect(resp_btwn_control_885nodes, 
                                                  resp_btwn_after_cutoff)
    length(resp_sel_in_control_imputed_btwn)
 
    btwn_control_imputed <- intersect(btwn_control_885nodes, btwn_after_cutoff)
    length(btwn_control_imputed)
-   
+
+   nonresp_btwn_control_885nodes <-intersect(btwn_control_885nodes, nonresp_btwn_after_cutoff)
+   length(nonresp_btwn_control_885nodes)
+       
    ## eigenvector centrality
    evcent_control_885nodes <- readRDS("../../control-net/control_885nodes_evcent_top300.RDS")
+   nonresp_evcent_after_cutoff <- evcent_after_cutoff[evcent_after_cutoff > 298]
 
    resp_evcent_control_885nodes <- evcent_control_885nodes[which(evcent_control_885nodes <= 298)]
 
@@ -28,8 +35,12 @@ load("resp_pcas_after_cutoffs.RData")
    evcent_control_imputed <- intersect(evcent_control_885nodes, evcent_after_cutoff)
    length(evcent_control_imputed)
 
+   nonresp_evcent_control_885nodes <-intersect(evcent_control_885nodes, nonresp_evcent_after_cutoff)
+   length(nonresp_evcent_control_885nodes)
+
    ## bridging
    bridging_control_885nodes <- readRDS("../../control-net/control_885nodes_bridging_top300.RDS")
+   nonresp_bridging_after_cutoff <- bridging_after_cutoff[bridging_after_cutoff > 298]
 
    resp_bridging_control_885nodes <- bridging_control_885nodes[which(bridging_control_885nodes <= 298)]
 
@@ -41,9 +52,12 @@ load("resp_pcas_after_cutoffs.RData")
                                          bridging_after_cutoff)
    length(bridging_control_imputed)
 
+   nonresp_bridging_control_885nodes <-intersect(bridging_control_885nodes, nonresp_bridging_after_cutoff)
+   length(nonresp_bridging_control_885nodes)
    
    ## key player
    kp_control_885nodes <- readRDS("../../control-net/control_885nodes_kp_set300.RDS")
+   nonresp_kp_after_cutoff <- kp_after_cutoff[kp_after_cutoff > 298]
 
    resp_kp_control_885nodes <- kp_control_885nodes[which(kp_control_885nodes <= 298)]
 
@@ -53,6 +67,9 @@ load("resp_pcas_after_cutoffs.RData")
 
    kp_control_imputed <- intersect(kp_control_885nodes, kp_after_cutoff)
    length(kp_control_imputed)
+
+   nonresp_kp_control_885nodes <-intersect(kp_control_885nodes, nonresp_kp_after_cutoff)
+   length(nonresp_kp_control_885nodes)
 
    ## overlaps in these pCA's on the control (i.e. control) and imputed networks
       ## btwn and all three
