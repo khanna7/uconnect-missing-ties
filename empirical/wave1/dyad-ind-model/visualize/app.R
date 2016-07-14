e809bd84c76255117c1b1b7c35ce9c07ae2c2c67
Aditya Khanna <khanna7@uchicago.edu>
2016-07-14 18:19:00 -0500

    visualize two networks

--- empirical/wave1/dyad-ind-model/visualize/app.R
+++ empirical/wave1/dyad-ind-model/visualize/app.R
@@ -13,30 +13,35 @@ load("data_to_plot_nets.RData")
 ui <- fluidPage(
   #titlePanel("Sampson's Monk dataset"),
   sidebarLayout(
-    sidebarPanel(       
+    sidebarPanel( 
+      selectInput("dataset", "Choose a dataset", 
+                  choices = c("Raw", "Imputed"),
+                  selected="Raw")
     ),
     mainPanel(
-      plotOutput("raw_net"),
-      br(), br(), br(), br(),
-      plotOutput("imp_to_plot")
+      plotOutput("net"),
+      br(), br(), br(), br()
+      #plotOutput("imp_to_plot")
      ## tableOutput("results")
     )
   )
 )
 
 server <- function(input, output) {
+
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Raw" = r0_net_no_isolates,
+           "Imputed" = imp_to_plot_no_isolates
+           ) 
+  })
   
-    output$raw_net <- renderPlot({
-      ggnet2(r0_net_no_isolates, size=1, color="respondent",
-             palette="Set2")+
-        ggtitle("Raw data")
-    })
-  
-    output$imp_to_plot <- renderPlot({
-      ggnet2(imp_to_plot_no_isolates, size=1, color="respondent",
-             palette="Set2"
-      )+ggtitle("Imputed data")    
-    })
+  output$net <- renderPlot({
+    #net_to_plot <- datasetInput()
+    ggnet2(datasetInput(), size=1, color="respondent", 
+          palette="Set2")
+  })
+
 }
 
 shinyApp(ui = ui, server = server)
