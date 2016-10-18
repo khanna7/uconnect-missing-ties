db3c3f6229fb5e4940e3814965f33b3283cd23fc
Aditya Khanna <khanna7@uchicago.edu>
2016-10-18 13:16:08 -0500

    editing typo

--- empirical/wave2/simple_stergm_on_r0.R
+++ empirical/wave2/simple_stergm_on_r0.R
@@ -3,7 +3,6 @@ library(tergm)
 library(network)
 
 load("/project/khanna7/Projects/UConnect/UConnect_FB/w1_w2_com_respondents_from_igraph/prep-awareness-use-and-fb-networks/pu-vs-ba-correct.RData")
-load("../wave1/imputed_matrix.RData)
 
 w1.com.resp.w.w2.data.ig
 w1.com.resp.w.w2.data.net
