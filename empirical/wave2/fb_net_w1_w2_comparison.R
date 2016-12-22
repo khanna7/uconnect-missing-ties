e002dd19bb6d86e41a157985239e7510c1584c13
Aditya Khanna <khanna7@uchicago.edu>
2016-12-22 10:41:37 -0600

    add distribution of R-NR. Also note drop in number of respondents after selection criteria is applied

--- empirical/wave2/fb_net_w1_w2_comparison.R
+++ empirical/wave2/fb_net_w1_w2_comparison.R
@@ -44,6 +44,29 @@ library(igraph)
    hist(deg_w1.fb.ig.10pc, breaks=seq(0, 800, 50), xlab="Wave 1")
    hist(deg_w2.fb.ig.10pc, breaks=seq(0, 800, 50), xlab="Wave 2")
 
+## How many respondents in each?
+   ## wave 1
+   w1_resp_10pc <- union(which(substr(V(w1.fb.ig.10pc)$name, 1, 4) == "1111"),
+                         which(substr(V(w1.fb.ig.10pc)$name, 1, 4) == "2222")
+                         )
+   length(w1_resp_10pc)
+
+   w1_resp <- union(which(substr(V(w1.fb.ig)$name, 1, 4) == "1111"),
+                          which(substr(V(w1.fb.ig)$name, 1, 4) == "2222")
+                    )
+   length(w1_resp)
+
+   ## wave 2
+   w2_resp_10pc <- union(which(substr(V(w2.fb.ig.10pc)$name, 1, 4) == "1111"),
+                         which(substr(V(w2.fb.ig.10pc)$name, 1, 4) == "2222")
+                         )
+   length(w2_resp_10pc)
+
+   w2_resp <- union(which(substr(V(w2.fb.ig)$name, 1, 4) == "1111"),
+                    which(substr(V(w2.fb.ig)$name, 1, 4) == "2222")
+                    )
+   length(w2_resp)
+
 ## ergm-related measures
 library(intergraph)
 library(network)
@@ -63,5 +86,40 @@ library(ergm)
    ## gwesp
       summary(w1.fb.net.10pc ~ triangle)
       summary(w2.fb.net.10pc ~ triangle)     
+
+## How many edges are R-R? How many are R-NR?
+   ## wave 1
+   w1_vnames <- w1.fb.net.10pc %v% "vertex.names"
+   w1_rds_seeds <- which(substr(w1_vnames, 1, 4) == "1111")
+   w1_rds_recruits <- which(substr(w1_vnames, 1, 4) == "2222")
+
+   w1_10pc_el <- as.edgelist(w1.fb.net.10pc)
+   w1_resp_resp_ties_id1 <- union(which(w1_10pc_el[,1] %in% w1_rds_seeds),
+                                  which(w1_10pc_el[,1] %in% w1_rds_recruits))
+   w1_resp_resp_ties_id2 <- union(which(w1_10pc_el[,2] %in% w1_rds_seeds),
+                                  which(w1_10pc_el[,2] %in% w1_rds_recruits))
+   w1_resp_resp_ties <- intersect(w1_resp_resp_ties_id1,
+                                  w1_resp_resp_ties_id2)
+
+   w1_resp_resp_el <- w1_10pc_el[w1_resp_resp_ties,]
+   dim(w1_resp_resp_el)
+   nrow(w1_10pc_el) - nrow(w1_resp_resp_el)
+  
+   ## wave 2
+   w2_vnames <- w2.fb.net.10pc %v% "vertex.names"
+   w2_rds_seeds <- which(substr(w2_vnames, 1, 4) == "1111")
+   w2_rds_recruits <- which(substr(w2_vnames, 1, 4) == "2222")
+
+   w2_10pc_el <- as.edgelist(w2.fb.net.10pc)
+   w2_resp_resp_ties_id1 <- union(which(w2_10pc_el[,1] %in% w2_rds_seeds),
+                                  which(w2_10pc_el[,1] %in% w2_rds_recruits))
+   w2_resp_resp_ties_id2 <- union(which(w2_10pc_el[,2] %in% w2_rds_seeds),
+                                  which(w2_10pc_el[,2] %in% w2_rds_recruits))
+   w2_resp_resp_ties <- intersect(w2_resp_resp_ties_id1,
+                                  w2_resp_resp_ties_id2)
+
+   w2_resp_resp_el <- w2_10pc_el[w2_resp_resp_ties,]
+   dim(w2_resp_resp_el)
+   nrow(w2_10pc_el) - nrow(w2_resp_resp_el)
 
 save.image(file="fb_net_comparison.RData")
