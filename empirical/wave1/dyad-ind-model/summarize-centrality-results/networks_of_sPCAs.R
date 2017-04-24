beea52d219cea46f4cc84bc5be33a41bb1cee876
Aditya Khanna <khanna7@uchicago.edu>
2017-04-24 09:00:06 -0500

    network computations for spcas

---
+++ empirical/wave1/dyad-ind-model/summarize-centrality-results/networks_of_sPCAs.R
@@ -0,0 +1,26 @@
+##############################
+## networks of sPCAs
+##############################
+rm(list=ls())
+
+## libraries
+library(igraph)
+load("pcas_after_cutoffs.RData")
+load("../../../wave1/control-net/centrality_for_control_net_w_885nodes.RData")
+
+## btwn
+btwn_nonresp_after_cutoff <- btwn_after_cutoff[btwn_after_cutoff > 298]
+summary(degree(w1.ig.deg.greq.30, v=btwn_nonresp_after_cutoff))
+
+## evcent
+evcent_nonresp_after_cutoff <- evcent_after_cutoff[evcent_after_cutoff > 298]
+summary(degree(w1.ig.deg.greq.30, v=evcent_nonresp_after_cutoff))
+
+##bridging
+bridging_nonresp_after_cutoff <- bridging_after_cutoff[bridging_after_cutoff > 298]
+summary(degree(w1.ig.deg.greq.30, v=bridging_nonresp_after_cutoff))
+
+##kp
+kp_nonresp_after_cutoff <- kp_after_cutoff[kp_after_cutoff > 298]
+summary(degree(w1.ig.deg.greq.30, v=kp_nonresp_after_cutoff))
+
